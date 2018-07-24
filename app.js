f108bae3affcdcb95edb09a4e93ad93e945c2a8c
Robert Burnat <burnat.robertt@gmail.com>
2018-07-24 18:09:02 +0200

    Add Reading input Data, and Data Object

--- app.js
+++ app.js
@@ -17,10 +17,28 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
+    //Objects to easier change the class data etc.
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
     
-    //Some code
-    
-    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        
+        //Make DOMstrings to the public method
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
     
 })();
 
@@ -28,9 +46,14 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
+    //Object DOMstrings from UIController
+    var DOM = UICtrl.getDOMstrings();
+    
     var ctrlAddItem = function() {
         
         //1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         //2. Add the item to the budget
 
@@ -39,7 +62,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         //4. Calculate the budget
 
         //5. Display the budget on the UI
-   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    }
+    
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function(event) {
         if(event.keyCode === 13 || event.which === 13) {
