81199f62be3afd31c2240caa18e61dc0bcf645ad
Robert Burnat <burnat.robertt@gmail.com>
2018-07-24 17:37:29 +0200

    Add new function, rebuild plan

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
-   document.querySelector('.add__btn').addEventListener('click', function() {
-      
-       // 1. Get the filed input data
+    var ctrlAddItem = function() {
+        
+        // 1. Get the filed input data
        
        //2. Add the item to the budget
        
@@ -39,11 +39,16 @@ var controller = (function(budgetCtrl, UICtrl) {
        //4. Calculate the budget
        
        //5. Display the budget on the UI
-       
-   });
+        console.log('It works.')
+        
+    }
+    
+   document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
     
     document.addEventListener('keypress', function(event) {
-        console.log(event);
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
     })
     
 })(budgetController, UIController);
