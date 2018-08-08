bce83ba6e8104bc83f5e5e5e122be84b899d6316
Robert Burnat <burnat.robertt@gmail.com>
2018-08-08 11:57:58 +0200

    Fix delete items

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import budgetController from './budgetController.js'
 import UIController from './UIController.js'
 
 //GLOBAL APP CONTROLLER
-let controller = ((budgetCtrl, UICtrl) => {
+let controller = (() => {
     //Object DOMstrings from UIController
-    let DOM = UICtrl.getDOMstrings();
+    let DOM = UIController.getDOMstrings();
     
     let setupEventListeners = () => {
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
@@ -18,32 +18,32 @@ let controller = ((budgetCtrl, UICtrl) => {
         
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
         
-        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+        document.querySelector(DOM.inputType).addEventListener('change', UIController.changedType);
         
     };
     
     let updateBudget = () => {
         
         //1. Calculate the budget
-        budgetCtrl.calculateBudget();
+        budgetController.calculateBudget();
         
         //2. Return  the budget
-        let budget = budgetCtrl.getBudget();
+        let budget = budgetController.getBudget();
 
         //3. Display the budget on the UI
-        UICtrl.displayBudget(budget);
+        UIController.displayBudget(budget);
     };
     
     var updatePercentages = () => {
         
         // 1. Calculate percentages
-        budgetCtrl.calculatePercentages();
+        budgetController.calculatePercentages();
         
         // 2. Read percentages from the budget controller
-        let percentages = budgetCtrl.getPercentages();
+        let percentages = budgetController.getPercentages();
         
         // 3. Update the UI with the new percentages
-        UICtrl.displayPercentages(percentages);
+        UIController.displayPercentages(percentages);
         
     }
     
@@ -51,17 +51,17 @@ let controller = ((budgetCtrl, UICtrl) => {
         let input, newItem;
         
         //1. Get the filed input data
-        input = UICtrl.getInput();
+        input = UIController.getInput();
         
         if(input.description !== "" && !isNaN(input.value) && input.value > 0) {
             //2. Add the item to the budget controller
-            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            newItem = budgetController.addItem(input.type, input.description, input.value);
 
             //3. Add the item to the UI
-            UICtrl.addListItem(newItem, input.type);
+            UIController.addListItem(newItem, input.type);
 
             //4. Clear the fields
-            UICtrl.clearFields();
+            UIController.clearFields();
 
             //5. Calculate and update budget
             updateBudget();    
@@ -75,7 +75,7 @@ let controller = ((budgetCtrl, UICtrl) => {
     
     let ctrlDeleteItem = event => {
         let itemID, splitID, type, ID;
-        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        itemID = event.target.parentNode.parentNode.parentNode.id;
         
         if(itemID) {
             
@@ -84,10 +84,10 @@ let controller = ((budgetCtrl, UICtrl) => {
             ID = splitID[1];
             
             // 1. delete the item from the data structure
-            budgetCtrl.deleteItem(type, ID)
+            budgetController.deleteItem(type, ID)
             
             // 2. Delete the item from the UI
-            UICtrl.deleteListItem(itemID);
+            UIController.deleteListItem(itemID);
             
             // 3. Update and show the new budget
             updateBudget();
@@ -101,8 +101,8 @@ let controller = ((budgetCtrl, UICtrl) => {
     return {
         init: () => {
             console.log('Application has started');
-            UICtrl.displayMonth();
-            UICtrl.displayBudget({
+            UIController.displayMonth();
+            UIController.displayBudget({
                 budget: 0,
                 totalInc: 0,
                 totalExp: 0,
@@ -112,7 +112,7 @@ let controller = ((budgetCtrl, UICtrl) => {
         }
     }
     
-})(budgetController, UIController);
+})();
 
 controller.init();
 
